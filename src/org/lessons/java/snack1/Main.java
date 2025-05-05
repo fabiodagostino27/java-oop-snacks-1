262877221f6045826fd3675b56f900c0673aa4d4
fabiodagostino27 <fabiodago27@gmail.com>
2025-05-05 17:09:57 +0200

    ADD: implement main method to create and display student information

--- src/org/lessons/java/snack1/Main.java
+++ src/org/lessons/java/snack1/Main.java
@@ -1,5 +1,9 @@
 package org.lessons.java.snack1;
 
 public class Main {
-
+    public static void main(String[] args) {
+        Studente mario = new Studente("Mario", "Rossi", 21);
+        
+        System.out.println(mario.getStudentInfo());
+    }
 }
