854eacc959b89e152d8ef31b42cac434a94a678c
fabiodagostino27 <fabiodago27@gmail.com>
2025-05-05 17:44:31 +0200

    ADD: implement RegistroStudenti class with constructor and static method to manage student record

--- src/org/lessons/java/snack3/Main.java
+++ src/org/lessons/java/snack3/Main.java
@@ -1,7 +1,12 @@
 package org.lessons.java.snack3;
 
+import org.lessons.java.snack1.Studente;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Studente pinco = new Studente("Pinco", "Pallo", 22);
+
+        System.out.println(pinco.getStudentInfo());
+        System.out.println(RegistroStudenti.getRegistro());
     }
 }
--- src/org/lessons/java/snack3/RegistroStudenti.java
+++ src/org/lessons/java/snack3/RegistroStudenti.java
@@ -1,5 +1,7 @@
 package org.lessons.java.snack3;
 
+import java.util.ArrayList;
+
 public class RegistroStudenti {
     // Snack 3: Registro Studenti
     // Crea una classe RegistroStudenti che rappresenti un registro di studenti.
@@ -7,4 +9,14 @@ public class RegistroStudenti {
     // Snack 1). Implementa un costruttore senza parametri per inizializzare il
     // registro vuoto e un metodo pubblico per aggiungere studenti al registro.
     // Aggiungi un metodo che stampi la lista degli studenti.
+
+    private static ArrayList<String> registro;
+
+    public RegistroStudenti() {
+        this.registro = new ArrayList<String>();
+    }
+
+    public static ArrayList<String> getRegistro() {
+        return registro;
+    }
 }
