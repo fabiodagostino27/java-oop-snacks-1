1a31e0b8b2b4063dbbff3d4d9d37391f34fb2aaf
fabiodagostino27 <fabiodago27@gmail.com>
2025-05-05 17:32:13 +0200

    ADD: create Main and RegistroStudenti classes with initial structure

---
+++ src/org/lessons/java/snack3/Main.java
@@ -0,0 +1,7 @@
+package org.lessons.java.snack3;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/org/lessons/java/snack3/RegistroStudenti.java
@@ -0,0 +1,10 @@
+package org.lessons.java.snack3;
+
+public class RegistroStudenti {
+    // Snack 3: Registro Studenti
+    // Crea una classe RegistroStudenti che rappresenti un registro di studenti.
+    // Utilizza un array privato per memorizzare oggetti Studente (creati nello
+    // Snack 1). Implementa un costruttore senza parametri per inizializzare il
+    // registro vuoto e un metodo pubblico per aggiungere studenti al registro.
+    // Aggiungi un metodo che stampi la lista degli studenti.
+}
