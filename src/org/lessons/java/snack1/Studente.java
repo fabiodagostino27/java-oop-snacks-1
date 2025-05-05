81e842699ad3cd524efd0a5f765649e3bd1f45c1
fabiodagostino27 <fabiodago27@gmail.com>
2025-05-05 17:05:00 +0200

    ADD: attributes

--- src/org/lessons/java/snack1/Studente.java
+++ src/org/lessons/java/snack1/Studente.java
@@ -1,5 +1,13 @@
 package org.lessons.java.snack1;
 
 public class Studente {
+    // Snack 1: Studente
+    // Scrivi una classe Studente con i seguenti attributi: nome, cognome, età.
+    // Implementa un costruttore che accetta tre parametri per inizializzare questi
+    // attributi. Aggiungi un metodo che restituisca una stringa concatenata dei
+    // valori (es. Anselmo Bitta, 24 anni).
 
+    private String nome;
+    private String cognome;
+    private int età;
 }
