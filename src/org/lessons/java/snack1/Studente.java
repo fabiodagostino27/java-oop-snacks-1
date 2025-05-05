b85659927901842e69271908dd8876270bdeeb62
fabiodagostino27 <fabiodago27@gmail.com>
2025-05-05 17:07:58 +0200

    ADD: constructor and getStudentInfo method

--- src/org/lessons/java/snack1/Studente.java
+++ src/org/lessons/java/snack1/Studente.java
@@ -10,4 +10,14 @@ public class Studente {
     private String nome;
     private String cognome;
     private int età;
+
+    public Studente(String nome, String cognome, int età) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.età = età;
+    }
+
+    public String getStudentInfo() {
+        return nome + " " + cognome + ", " + età + " anni.";
+    }
 }
